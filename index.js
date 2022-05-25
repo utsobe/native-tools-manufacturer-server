03a1eecbb762bb7ded0e720a6008fc124a3018d0
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-05-25 23:56:38 +0600

    create-payment-intent api created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,7 +29,7 @@ function verifyJWT(req, res, next) {
         req.decoded = decoded;
         next();
     });
-}
+};
 
 async function run() {
     try {
@@ -36,6 +37,19 @@ async function run() {
         const toolCollection = client.db('native_tools').collection('tools');
         const orderCollection = client.db('native_tools').collection('orders');
         const userCollection = client.db('native_tools').collection('users');
+
+        // stripe
+        app.post('/create-payment-intent', async (req, res) => {
+            const order = req.body;
+            const price = order.orderValue;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
 
         // get all tools 
         app.get('/tool', async (req, res) => {
