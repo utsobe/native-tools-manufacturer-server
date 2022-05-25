26a85c8d675ee100ee1feb646603a22a3b0fe519
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-05-25 17:20:21 +0600

    post order API created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,13 +18,29 @@ async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('native_tools').collection('tools');
+        const orderCollection = client.db('native_tools').collection('orders');
 
+        // get all tools 
         app.get('/tool', async (req, res) => {
-            console.log('database connected');
             const query = {};
             const tools = await toolCollection.find(query).toArray();
             res.send(tools);
-        })
+        });
+
+        // get single tool by id 
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        });
+
+        // post order
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
     }
     finally {
 
